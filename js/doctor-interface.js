e6d9c2f6c3fe0813ed266aeccc06968f31ce3087
Alex Francois <apfrancois86@gmail.com>
2017-01-06 09:46:01 -0800

    add Search object

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,11 @@
-var Doctors = require('./../js/doctor.js').getDoctors;
+var Doctors = require('./../js/doctor.js').doctorSearch;
 
 $(document).ready(function(){
   $('.doctor').submit(function(event){
-    // event.preventDefault();
-    // var medicalIssue = $('symptom').val();
-    // $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' +medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response){
-    //   console.log(response);
-    // }).fail(function(error){
-    //      console.log("fail");
-    //   });
+    event.preventDefault();
+    var currentSearch = new Search();
+    var symptom = $('#sypmtom').val();
+    currentSearch.getDoctors(symptom);
+    console.log(symptom);
   });
 });
