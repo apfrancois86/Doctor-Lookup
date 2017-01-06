26237ee929a098f495250e01e0b704d51f3cea19
Alex Francois <apfrancois86@gmail.com>
2017-01-06 12:27:35 -0800

    place results in individual divs

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,12 +2,13 @@ var Search = require('./../js/doctor.js').doctorSearch;
 
 var Doctor = function(searchResults) {
   for(var i =0; i<searchResults.data.length; i++){
-    $('.results').append('<h2>' + searchResults.data[i].profile.first_name + " " + searchResults.data[i].profile.last_name + '</h2>' + '<li>' + searchResults.data[i].profile.bio + '</li>');
+    $('.results').append('<div class="doctorResults">'+ '<h2>' + searchResults.data[i].profile.first_name + " " + searchResults.data[i].profile.last_name + '</h2>' + '<li>' + searchResults.data[i].profile.bio + '</li>'+ '</div>');
   }
 };
 
 $(document).ready(function(){
   $('.doctor').submit(function(event){
+    $('.results').empty();
     event.preventDefault();
     var currentSearch = new Search();
     var symptom = $('#symptom').val();
