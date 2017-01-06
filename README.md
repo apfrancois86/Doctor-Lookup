3ca8d76d6e907c22ee167af48a27f4a0b6a96e70
Alex Francois <apfrancois86@gmail.com>
2017-01-06 13:47:38 -0800

    add README file

---
+++ README.md
@@ -0,0 +1,26 @@
+# Doctor Lookup
+
+### Created by Alex Francois
+
+
+## Description
+This is a website where users may enter a medical issue (ie: “toothache”) into a form, submit it, and receive a list of doctors they may seek out to help with their medical issue.
+
+
+## Setup
+Clone directory, run "npm install", run 'bower install', and edit HTML, CSS, and JS files as needed. To display in browser run "gulp build" and then "gulp serve".
+
+## Technologies Used
+* HTML
+* CSS
+* Javascript
+* jQuery
+* Node.js
+* NPM
+* Gulp
+* Bower
+* Git
+* Command Line
+
+## Legal
+Copyright 2016 GPL
--- js/doctor.js
+++ js/doctor.js
@@ -3,6 +3,7 @@ var apiKey = require('./../.env').apiKey;
 function Search(){
   var medicalIssue;
   var searchResults;
+  var displayDoctors;
 }
 
 Search.prototype.getDoctors = function (medicalIssue, displayDoctors) {
@@ -10,7 +11,7 @@ Search.prototype.getDoctors = function (medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
     console.log(result);
-    this.searchResults=result;
+    this.searchResults=result
     displayDoctors(result);
   })
   .fail(function(error){
