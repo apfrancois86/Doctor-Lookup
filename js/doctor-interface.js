3a69dc32ed2ed9db08aa79f108ce22703672b8fb
Alex Francois <apfrancois86@gmail.com>
2017-01-06 11:38:52 -0800

    add bio output

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,8 +2,7 @@ var Search = require('./../js/doctor.js').doctorSearch;
 
 var Doctor = function(searchResults) {
   for(var i =0; i<searchResults.data.length; i++){
-    $('.results').append('<li>' + searchResults.data[i].profile.first_name + " " + searchResults.data[i].profile.last_name + '</li>' );
-    console.log(searchResults);
+    $('.results').append('<h2>' + searchResults.data[i].profile.first_name + " " + searchResults.data[i].profile.last_name + '</h2>' + '<li>' + searchResults.data[i].profile.bio + '</li>');
   }
 };
 
