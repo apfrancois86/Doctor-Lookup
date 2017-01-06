3acf85d4eb3eaf991b28992e3f2d2d09bd53516d
Alex Francois <apfrancois86@gmail.com>
2017-01-06 11:31:19 -0800

    add working function to display results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,18 @@
-var Doctors = require('./../js/doctor.js').doctorSearch;
+var Search = require('./../js/doctor.js').doctorSearch;
+
+var Doctor = function(searchResults) {
+  for(var i =0; i<searchResults.data.length; i++){
+    $('.results').append('<li>' + searchResults.data[i].profile.first_name + " " + searchResults.data[i].profile.last_name + '</li>' );
+    console.log(searchResults);
+  }
+};
 
 $(document).ready(function(){
   $('.doctor').submit(function(event){
     event.preventDefault();
     var currentSearch = new Search();
-    var symptom = $('#sypmtom').val();
-    currentSearch.getDoctors(symptom);
-    console.log(symptom);
+    var symptom = $('#symptom').val();
+    currentSearch.getDoctors(symptom, Doctor);
+    console.log(currentSearch);
   });
 });
