9f54387534f89ac50ccb0f1c536ab38cd3683646
Alex Francois <apfrancois86@gmail.com>
2017-01-06 08:48:53 -0800

    add starter code provided to begin testing

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,2 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
+
+$(document).ready(function(){
+  var medicalIssue = $('symptom').val();
+  exports.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
+});
