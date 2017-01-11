6fe36b24f43ad6371959ddbf9102f923cadd9063
Alex Francois <apfrancois86@gmail.com>
2017-01-10 18:36:56 -0800

    change function name to be more descriptive

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,6 @@
 var Search = require('./../js/doctor.js').doctorSearch;
 
-var Doctor = function(searchResults) {
+var displayDoctorInfo = function(searchResults) {
   for(var i =0; i<searchResults.data.length; i++){
     $('.results').append('<div class="doctorResults">'+ '<h2>' + searchResults.data[i].profile.first_name + " " + searchResults.data[i].profile.last_name + '</h2>' + '<img class="doctorImage" src=' + searchResults.data[i].profile.image_url + '>' + '<li>' + searchResults.data[i].profile.bio + '</li>'+ '</div>');
   }
@@ -12,6 +12,6 @@ $(document).ready(function(){
     event.preventDefault();
     var currentSearch = new Search();
     var symptom = $('#symptom').val();
-    currentSearch.getDoctors(symptom, Doctor);
+    currentSearch.getDoctors(symptom, displayDoctorInfo);
   });
 });
